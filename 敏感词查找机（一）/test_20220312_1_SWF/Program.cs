dc2d96de5a637e6befb582e991ddb71af0ca561d
Thirra-Fang <97672737+Thirra-Fang@users.noreply.github.com>
2022-03-12 18:42:17 +0800

    Add files via upload

---
+++ 敏感词查找机（一）/test_20220312_1_SWF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test_20220312_1_SWF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            str = str.Replace("Hello world","Hello <你的名字>");
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ 敏感词查找机（一）/test_20220312_2_SWF2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace test_20220312_2_SWF2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // 创建一个 StreamReader 的实例来读取文件 
+                // using 语句也能关闭 StreamReader
+                using (StreamReader sr = new StreamReader("输入.txt"))
+                {
+                    StreamWriter sw = new StreamWriter("输出.txt");
+                    String line;
+                    // 从文件读取并显示行，直到文件的末尾 
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        line = line.Replace("Hello world","Hello <你的名字>");
+                        sw.WriteLine(line);
+                    }
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                // 向用户显示出错消息
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
